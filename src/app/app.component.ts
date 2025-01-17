cfc163002d667b324e8d4779e5c828cb34abfe30
Silvia Urraca <134202554+junhoes@users.noreply.github.com>
2025-01-17 14:28:20 +0100

    basic code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TareasService } from './services/tareas.service';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'to-do-list';
+export class AppComponent implements OnInit{
+  title = 'To do list';
+  taskList: string[] = [];
+  newTask: string = '';
+
+  weekDays: string[] = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+  date: Date = new Date();  
+
+  private _tareasService = inject(TareasService); // inyección del servicio tareas
+  
+  ngOnInit(): void {
+    this.taskList = this._tareasService.getTasks(); // importamos las tareas almacenadas en localStorage
+  }
+
+  addTask(){
+    this._tareasService.addTask(this.newTask);
+    this.newTask = '';  // se vacía newTask para la siguiente tarea que se añada
+    this.taskList = this._tareasService.getTasks(); // actualizamos la lista de tareas
+  }
+
+  deleteTask(index: number){
+    this._tareasService.deleteTask(index);
+    this.taskList = this._tareasService.getTasks(); // actualizamos la lista de tareas
+  }
+
+  // DATE METHODS
+  getWeekDay():string {
+    let day = this.date.getDay();
+    return this.weekDays[day];
+  }
+
+  getSimpleDate():string {
+    const day = this.date.getDate().toString().padStart(2, '0');
+    const month = (this.date.getMonth() + 1).toString().padStart(2, '0'); 
+    const year = this.date.getFullYear().toString().slice(-2); 
+  
+    return `${day}/${month}/${year}`;
+  }
+  
 }
---
+++ src/app/services/tareas.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TareasService {
+
+  private localStorageKey = 'taskList';
+
+  private isLocalStorageAvailable(): boolean {
+    return typeof window !== 'undefined' && !!window.localStorage;
+  }
+
+  getTasks(): string[] {
+    if (this.isLocalStorageAvailable()) {
+      return JSON.parse(localStorage.getItem(this.localStorageKey) as string) || [];
+    }
+    return []; // Devuelve un arreglo vacío si no está disponible
+  }
+
+  addTask(task: string) {
+    if (this.isLocalStorageAvailable()) {
+      const tasks = this.getTasks();
+      tasks.push(task);
+      localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+    }
+  }
+
+  deleteTask(index: number){
+    if (this.isLocalStorageAvailable()) {
+      const tasks = this.getTasks();
+      tasks.splice(index, 1); // Eliminamos del array 1 elemento desde el index
+      localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+    }
+  }
+}
